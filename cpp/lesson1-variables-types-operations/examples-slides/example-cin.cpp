7c6e4c14ddf39813b8cde303496496bdf5f52250
Rafaela Schroeder <rafaela.kschroeder@gmail.com>
2025-09-12 16:05:56 +0300

    Add examples from slides

---
+++ cpp/lesson1-variables-types-operations/examples-slides/example-cin.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+  int num; // first initialize the variable
+  std::cout << "Please input your number: ";
+  std::cin >> num; // we read the number to variable "num"
+  std::cout << "Your number is " << num << " \n";
+  return 0;
+}
